deb4615a60f9700020a4e5ec000314ff04c34175
Muhammad Dhika Azizi <mdhikas@gmail.com>
2021-01-16 22:10:08 +0700

    first commit

---
+++ app/Views/templates/sidebar.php
@@ -0,0 +1,69 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-code"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">WPU</div>
+    </a>
+
+    <?php if (in_groups('admin')) : ?>
+        <!-- Divider -->
+        <hr class="sidebar-divider">
+
+        <!-- Heading -->
+        <div class="sidebar-heading">
+            User Management
+        </div>
+
+
+        <!-- Nav Item - User -->
+        <li class="nav-item">
+            <a class="nav-link" href="<?= base_url('admin'); ?>">
+                <i class="fas fa-users"></i>
+                <span>User List</span></a>
+        </li>
+    <?php endif; ?>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        User Profile
+    </div>
+
+
+    <!-- Nav Item - My Profile -->
+    <li class="nav-item">
+        <a class="nav-link" href="<?= base_url('user'); ?>">
+            <i class="fas fa-user"></i>
+            <span>My Profile</span></a>
+    </li>
+
+    <!-- Nav Item - Tables -->
+    <li class="nav-item">
+        <a class="nav-link" href="tables.html">
+            <i class="fas fa-user-edit"></i>
+            <span>Edit Profile</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Nav Item - Tables -->
+    <li class="nav-item">
+        <a class="nav-link" href="<?= base_url('logout'); ?>">
+            <i class="fas fa-sign-out-alt"></i>
+            <span>Logout</span></a>
+    </li>
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+</ul>
---
+++ app/Views/user/index.php
@@ -0,0 +1,9 @@
+<?= $this->extend('templates/index'); ?>
+<?= $this->section('page-content'); ?>
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800">My Profile</h1>
+
+</div>
+<?= $this->endSection(); ?>
